dffedd67252e56f498203e0a1e9e5585c45c7668
SMN Shuvo <smnshuvo@pop-os.localdomain>
2023-04-01 00:35:53 +0600

    Create a basic chat UI with animation

--- lib/screens/one_to_one_chat_screen.dart
+++ lib/screens/one_to_one_chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'list_item.dart';
+
+part 'chat_list_item.dart';
 
 class OneToOneChatScreen extends StatefulWidget {
   const OneToOneChatScreen({super.key});
@@ -11,13 +12,64 @@ class OneToOneChatScreen extends StatefulWidget {
 
 class _OneToOneChatScreenState extends State<OneToOneChatScreen> {
   final TITLE = "Demo Chat Screen";
+  final _listKey = GlobalKey<AnimatedListState>();
+  late ListModel<int> _list;
+  late int _nextItem;
+
+  @override
+  void initState() {
+    _list =
+        ListModel<int>(listKey: _listKey, removedItemBuilder: _buildRemoveItem);
+    _nextItem = _list.length;
+    super.initState();
+  }
+
+  void _addChatItem() {
+    print("Inserted at $_nextItem");
+    _list.insert(_nextItem, _nextItem++);
+  }
+
+  void _removeLastChatItem() async {
+    _list.removeLastItem();
+    _nextItem--;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(TITLE),
+        actions: [
+          IconButton(onPressed: _addChatItem, icon: Icon(Icons.add)),
+          IconButton(onPressed: _removeLastChatItem, icon: Icon(Icons.remove))
+        ],
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: AnimatedList(
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+          key: _listKey,
+          initialItemCount: _list.length,
+          itemBuilder: _buildItem,
+        ),
+      ),
     );
+  }
+
+  Widget _buildItem(
+    BuildContext context,
+    int index,
+    Animation<double> animation,
+  ) {
+    return ChatListItem(animation: animation, chatId: _list[index]);
+  }
+
+  Widget _buildRemoveItem(
+    BuildContext context,
+    int index,
+    Animation<double> animation,
+  ) {
+    return ChatListItem(animation: animation, chatId: _list[index]);
   }
 }
