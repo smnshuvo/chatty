f52a1d566807996faa06b5ce6685d66c83e5326b
SMN Shuvo <smnshuvo@gmail.com>
2023-04-01 13:11:41 +0600

    Merged sender, receiver animation as I think animation should have a consistency

--- lib/screens/chat_list_item.dart
+++ lib/screens/chat_list_item.dart
@@ -39,7 +39,7 @@ class _ChatListItemState extends State<ChatListItem>
 
     receiverScaleAnimation = CurvedAnimation(
       parent: controller,
-      curve: Curves.easeInOutBack,
+      curve: Curves.elasticInOut,
     );
 
     sizeAnimation = CurvedAnimation(
